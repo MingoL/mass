001962ba945afd9ffa9ade03b1c7136cc70edf93
PowerSupply <sly.007.2000@gmail.com>
2019-07-18 11:24:35 +0800

    Commit 1

---
+++ Mass.py
@@ -0,0 +1,53 @@
+#Color Setting
+W  = '\033[0m'  # white (normal)
+R  = '\033[31m' # red
+G  = '\033[32m' # green
+O  = '\033[33m' # orange
+B  = '\033[34m' # blue
+P  = '\033[35m' # purple
+
+def Mass():
+	try:
+		print(G+"MASS"+W)
+		BagWidth    = int(input("Width mm:"))
+		BagLength   = int(input("Length mm:"))
+		Thickness   = int(input("Thickness micron:"))
+		Density     = float(input("Density:"))
+		sum         = BagLength*BagWidth*Thickness*Density/1000000
+		print(G+"Weight of the Bag in Gramme is: "+W, sum)
+	except ValueError:
+		return
+
+def BUR():
+	try:
+		print(G+"BUR"+W)
+		DieSize		= int(input("Die Size mm:"))
+		BagWidth    = int(input("Width mm:"))
+		sum         = BagWidth/1.57/DieSize
+		print(G+"BUR is: "+W, sum)
+	except ValueError:
+		return
+
+def main():
+	exit = False
+	while not exit:
+		print("1 = Mass")
+		print("2 = BUR")
+		print("3 = Exit Program")
+		C = int(input(O+"option ="+W))
+		if C == 1:
+			Mass()
+		elif C == 2:
+			BUR()
+		elif C == 3:
+			exit = True
+			print("Exiting Program")
+			#break
+		print(R+"Enter a number"+W)
+			#else:
+			#	print("enter a number")
+			#TEST1
+		
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mass
+
+This is my first time using Git and VSCode.
+
+Simple BUR calculation
+1CH
+
+-----------------------------------------------------------
+吃葡萄不吐葡萄皮 不吃葡萄倒吐葡萄皮
+Tonton, ton thé t’a-t-il ôté ta toux?
+
